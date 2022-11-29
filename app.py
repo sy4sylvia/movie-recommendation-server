766ef824b266b1b4c79efd29a79bd46a59647118
sy4sylvia <sguo713@gmail.com>
2022-11-28 22:23:03 -0500

    feature: implement get movie recommendation with user id

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import pandas as pd
 import numpy as np
@@ -37,17 +37,37 @@ def api_profile():  # put application's code here
 def post_test():
     req_data = request.get_json()
 
-    _userId = req_data.get("userId")
+    _userId = req_data.get('userId')
 
     print(type(_userId))
 
-    if _userId == 1:
+    # begin processing...
+    user_rated_movies_df = ratings_df.loc[ratings_df['userId'].isin([_userId])]
+    # existing_user_movies = genres[genres['movieId'].isin(user_rated_movies_df['movieId'].tolist())]
+
+    existing_user_movies = movies_with_genres_df[
+        movies_with_genres_df['movieId'].isin(user_rated_movies_df['movieId'].tolist())]
+    existing_user_genres = existing_user_movies.drop(columns=['movieId', 'title', 'genres', 'year'])
+    existing_user_profile = np.matmul(existing_user_genres.transpose(), user_rated_movies_df['rating'])
+
+    existing_recommendation_df = ((genres * existing_user_profile).sum(axis=1)) / (existing_user_profile.sum())
+    # Recommendation System, sort the weighted average descending
+    existing_recommendation_df = existing_recommendation_df.sort_values(ascending=False)
+
+    existing_top_reco = movies_df.loc[movies_df['movieId'].isin(existing_recommendation_df.head(30).keys())]
+    # Filter out the movies that the existing user has already rated
+    existing_top_reco = existing_top_reco[~existing_top_reco['movieId'].isin(existing_user_movies['movieId'])]
+
+    # convert into JSON objects
+    result = existing_top_reco.to_json(orient="records")
+    parsed = json.loads(result)
+    json.dumps(parsed, indent=4)
+
+    if _userId < 0:
         return {"success": False,
-                "msg": "Email already taken"}, 400
+                "msg": "Not a valid user id"}, 400
     else:
-        return {"success": True,
-                # "userID": new_user.id,
-                "msg": "The user was successfully registered"}, 200
+        return parsed, 200
 
 
 # recommendation route
@@ -64,7 +84,7 @@ def create_recommendation(givenId):
     existing_user_profile = np.matmul(existing_user_genres.transpose(), user_rated_movies_df['rating'])
 
     existing_recommendation_df = ((genres * existing_user_profile).sum(axis=1)) / (existing_user_profile.sum())
-    # Recommendation System, sort the weighted average descendingly
+    # Recommendation System, sort the weighted average descending
     existing_recommendation_df = existing_recommendation_df.sort_values(ascending=False)
 
     existing_top_reco = movies_df.loc[movies_df['movieId'].isin(existing_recommendation_df.head(50).keys())]
