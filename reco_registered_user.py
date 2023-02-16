2d9888a093d9bafd3486aac00e5f0d7ea89f5984
sy4sylvia <sguo713@gmail.com>
2023-02-16 14:37:40 -0800

    chore: remove comments

--- reco_registered_user.py
+++ reco_registered_user.py
@@ -11,13 +11,13 @@ movies = pd.read_csv("dataset/movies.csv", header=0)
 
 def get_recommendation():
     user_rated_movies_df = ratings_df.loc[ratings_df['userId'].isin([1])]
-    # existing_user_movies = genres[genres['movieId'].isin(user_rated_movies_df['movieId'].tolist())]
-
+    
     existing_user_movies = movies_with_genres_df[movies_with_genres_df['movieId'].isin(user_rated_movies_df['movieId'].tolist())]
     existing_user_genres = existing_user_movies.drop(columns = ['movieId', 'title', 'genres', 'year'])
     existing_user_profile = np.matmul(existing_user_genres.transpose(), user_rated_movies_df['rating'])
 
     existing_recommendation_df = ((genres * existing_user_profile).sum(axis = 1)) / (existing_user_profile.sum())
+    
     # Recommendation System, sort the weighted average descendingly
     existing_recommendation_df = existing_recommendation_df.sort_values(ascending = False)
 
